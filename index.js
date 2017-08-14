7458118f104c9db9661b0cd31c3bc1c1e729df9c
Kip Gibson <Kiptong@gmail.com>
2017-08-14 10:19:34 -0700

    Adding devDependencies. #1

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.json())
+
+app.listen('3000', () => {
+  console.log('Listening on 3000.')
+})
